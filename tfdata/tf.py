5ee9d70ed8dc15c939829ca4843a4d946e872ddd
Panini <mumuxi15@icloud.com>
2021-05-22 01:39:39 -0400

    initial commit

--- tfdata/tf.py
+++ tfdata/tf.py
@@ -1,13 +1,12 @@
-import os
-import json
+import aiohttp
 import asyncio
-import hashlib
 import requests
+import time
 import pandas as pd
-#from .config import *
 from config import *
+import sys
 
-from time import time
+
 
 # #########   Exceptions     #############
 
@@ -31,83 +30,84 @@ class AuthTokenError(AuthException):
 		msg = super().__str__()
 		return 'Token error: {0}'.format(msg)
 
+
+
 # #########   API    #############
 class APIClient(object):
 
-	session = requests.Session()
-
-	def __init__(self, username, password, base_url="https://www.trounceflow.com/api/v1/"):  ###client_key, secret_key
+	def __init__(self, username, password, 
+				 base_url="https://www.trounceflow.com/api/v1/"): 
 		"""Initialize an APIClient object."""
 		self.base_url = base_url
 		self.username = username
 		self.password = password
 		self.access_token = None
 		self.refresh_token = None
-
-		self.__get_access_token()
-
-#		self.credentials = Credentials(client_key=client_key, secret_key=secret_key)
-	def sign(self, key, msg):
-		"""Get the digest of the message using the specified key."""
-		return hmac.new(key.encode(), msg.encode(), hashlib.sha256).hexdigest()
-
-
+		self.access_token = asyncio.run(self.get_access_token())['token']
+#		self.__get_access_token()
+		
 	def __get_access_token(self):
-		""" create token """
+		""" create token """	
 		response = requests.post(
 			url  = self.base_url + "auth-token/",
 			data = {
 				'username': self.username,
 				'password': self.password
-			}
+				}
 		)
 		try:
 			response.raise_for_status()
 			self.access_token = response.json()['token']
-			# additional code will only run if request is successful
+			response.close()
 		except requests.exceptions.RequestException as err_http:
 			raise SystemExit('Access Denied: %s '%(err_http))
 		except requests.exceptions.ConnectionError as err_cnn:
 		    raise SystemExit('Connection Failed: %s '%(err_cnn))
 		except requests.exceptions.Timeout as err_timeout:
 		    raise SystemExit('Timeout: %s '%(err_timeout))
+		
+	async def get_access_token(self):
+		async with aiohttp.ClientSession() as session:
+			async with session.post(self.base_url + "auth-token/", 
+					   data= {'username': self.username,'password': self.password}) as response:				
+				return await response.json()
+		
+	async def fetch_ticker(self, session, ticker):
+		url = URL_MAPPER[ticker]
 
-	def get_main_network_headers():
-	    headers = {
-	        "nethash": "bc669a7d8a4ce656d1acd326d694ef6a45ae181e93b57505170bcc26ddb7b98c", #hashed address
-	        "version": "1.0.1",
-	        "port": "4001"
-	    }
-	    return headers
-
-	def set_token(self,token=None):  # this is a bypass, potential security prob ???
-		self.access_token = token
-
-	def refresh_token(self):
-		self.__get_access_token()
-
-	def __load_ticker(self,ticker):
-		response = requests.get(URL_MAPPER[ticker], headers= {'Authorization': 'Token '+self.access_token})
-		if response.ok:
-			df = pd.DataFrame(json.loads(response.text)).set_index('date')
-			df.index = pd.to_datetime(df.index)
-			return df
+		async with session.get(url, 
+			headers= {'Authorization': 'Token '+self.access_token}) as response:
+			results = await response.json()
+			return pd.DataFrame(results).set_index('date').add_prefix(ticker.split(':')[0]+'_')
+			
+	async def __get_resource(self,tickers):
+		async with aiohttp.ClientSession() as session:
+			tasks = []
+			for ticker in tickers:
+				task = asyncio.ensure_future(self.fetch_ticker(session, ticker))
+				tasks.append(task)
+			rs = await asyncio.gather(*tasks)
+			df = pd.concat(rs,axis=1)
+		return df
+		
+	def get_resource(self,tickers, to_csv=None):
+		if isinstance(tickers, list):
+			df = asyncio.run(self.__get_resource(tickers))
+		elif isinstance(tickers,str):
+			df = asyncio.run(self.__get_resource([tickers]))
 		else:
-			print ('Data request denied,  {0}'.format(URL_MAPPER[ticker]))
-			return pd.DataFrame()
+			return 'tickers type str or list'
 
-
-	def load_data(self,tickers,start_date=None,end_date=None):
-		if self.access_token:			
-			df = list(map(self.__load_ticker, tickers))
-			df = pd.concat(df,axis=1)
-#			print (df)
-			df = df.apply(pd.to_numeric)
-			if start_date:
-				df = df.loc[df.index>start_date]
-			if end_date:
-				df= df.loc[df.index<end_date]
-		else:
-			return 'Token Error, please use set_token(token='') function'
-
-
+		if to_csv:
+			df.to_csv(to_csv)
+		return df
+		
+	def get_direct_url(self,ticker):
+		return URL_MAPPER[ticker]
+	
+	def get_meta_info(self,ticker):
+		return
+		
+	def get_freq(self,ticker):
+		return 
+		#pd.infer_freq(index, warn=True)
